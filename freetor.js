011ea9a68dc8af822e0827a4073d44ce8bf44974
lysiy <vazonez1982@gmail.com>
2025-02-08 18:28:07 +0300

    freetor.js

--- freetor.js
+++ freetor.js
@@ -1477,9 +1477,9 @@
     Lampa.Manifest.plugins = manifst;
     Lampa.Lang.add({
       lampac_watch: { //
-        ru: 'Онлайн',
-        en: 'Watch online',
-        uk: 'Дивитися онлайн',
+        ru: 'Онлайн 4am1k',
+        en: 'Online 4am1k',
+        uk: 'Онлайн 4am1k',
         zh: '在线观看'
       },
       lampac_video: { //
