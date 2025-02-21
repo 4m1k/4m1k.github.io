7ca586247643df446442abd2f20ede7c04c299f1
lysiy <vazonez1982@gmail.com>
2025-02-22 01:40:10 +0300

    test2.js

--- test2.js
+++ test2.js
@@ -1,43 +1,86 @@
 (function () {
-    console.log("Блокировка рекламы активирована");
+  'use strict';
 
-    // Подменяем проверку подписки (премиум аккаунт)
-    window.Account = window.Account || {};
-    window.Account.hasPremium = () => true;
+  Lampa.Platform.tv();
 
-    // Ломаем создание <video> для рекламы
-    document.createElement = new Proxy(document.createElement, {
-        apply(target, thisArg, args) {
-            if (args[0] === "video") {
-                console.log("Перехватываем создание <video> для рекламы!");
-
-                let fakeVideo = target.apply(thisArg, args);
-
-                // Запрещаем рекламе воспроизводиться
-                fakeVideo.play = function () {
-                    console.log("Рекламное видео заблокировано!");
-                    setTimeout(() => {
-                        fakeVideo.ended = true;
-                        fakeVideo.dispatchEvent(new Event("ended")); // Эмулируем завершение рекламы
-                    }, 500);
-                };
-
-                return fakeVideo;
-            }
-            return target.apply(thisArg, args);
-        }
-    });
-
-    // Очищаем таймеры рекламы
-    function clearAdTimers() {
-        console.log("Очищаем рекламные таймеры...");
-        let highestTimeout = setTimeout(() => {}, 0);
-        for (let i = 0; i <= highestTimeout; i++) {
-            clearTimeout(i);
-            clearInterval(i);
-        }
+  function overrideConsole() {
+    var globalEnv;
+    try {
+      globalEnv = Function("return (function() {}.constructor('return this')());")();
+    } catch (error) {
+      globalEnv = window;
     }
+    
+    var logMethods = ["log", "warn", "info", "error", "exception", "table", "trace"];
+    var logConsole = globalEnv.console = globalEnv.console || {};
 
-    // Убираем рекламу после загрузки страницы
-    document.addEventListener("DOMContentLoaded", clearAdTimers);
+    logMethods.forEach(function (method) {
+      var defaultLogMethod = logConsole[method] || function () {};
+      logConsole[method] = function () {
+        return defaultLogMethod.apply(logConsole, arguments);
+      };
+    });
+  }
+  
+  overrideConsole();
+
+  var menuCategories = [
+    { title: "Русские фильмы", url: "?cat=movie&airdate=2023-2025&without_genres=16&language=ru" },
+    { title: "Русские сериалы", url: "discover/tv?&with_original_language=ru" },
+    { title: "Русские мультфильмы", url: "?cat=movie&airdate=2020-2025&genre=16&language=ru" },
+    { title: "Okko", url: "discover/tv?network=3871" },
+    { title: "КиноПоиск", url: "discover/tv?network=3502" },
+    { title: "Wink", url: "discover/tv?network=5024" },
+    { title: "Start", url: "discover/tv?network=2493" },
+    { title: "Premier", url: "discover/tv?network=2859" },
+    { title: "ИВИ", url: "discover/tv?network=3923" },
+    { title: "KION", url: "discover/tv?network=4085" },
+    { title: "СТС", url: "discover/tv?network=4009" },
+    { title: "ТНТ", url: "discover/tv?network=4031" }
+  ];
+
+  Lampa.Select.show({
+    title: Lampa.Lang.translate('settings_rest_source'),
+    items: menuCategories,
+    onSelect: function (chosenCategory) {
+      Lampa.Activity.push({
+        url: chosenCategory.url,
+        title: chosenCategory.title,
+        component: 'category_full',
+        source: 'cub',
+        card_type: 'true',
+        page: 1
+      });
+    }
+  });
+
+  function initializeSettings() {
+    Lampa.SettingsApi.addParam({
+      component: "interface",
+      param: {
+        name: "rus_movie_main",
+        type: "trigger",
+        default: true
+      },
+      field: {
+        name: "Русские новинки на главной",
+        description: "Показывать подборки русских новинок на главной странице. После изменения параметра приложение нужно перезапустить"
+      },
+      onRender: function (element) {
+        setTimeout(function () {
+          $("div[data-name=\"rus_movie_main\"]").insertAfter("div[data-name=\"interface_size\"]");
+        }, 0);
+      }
+    });
+  }
+
+  if (window.appready) {
+    initializeSettings();
+  } else {
+    Lampa.Listener.follow("app", function (event) {
+      if (event.type == "ready") {
+        initializeSettings();
+      }
+    });
+  }
 })();
