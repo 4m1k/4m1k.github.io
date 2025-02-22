ee4e42b95126e4e3d239b4592470b85e1d697f74
lysiy <vazonez1982@gmail.com>
2025-02-22 19:24:41 +0300

    test2.js

--- test2.js
+++ test2.js
@@ -14,22 +14,25 @@
 
   /**
    * Массив обфусцированных строк.
-   * (Пример сокращённого массива – полный массив может содержать гораздо больше элементов)
+   * Добавьте в этот массив все необходимые строки, используемые в коде.
    */
   function getObfuscationArray() {
     return [
-      "table",
-      "79.137.204.8:9117",
-      "Ошибка",
-      "Error:",
-      "Jacred.xyz",
-      "jacred_viewbox_dev",
-      "change",
-      "field",
-      "ff2121",
-      "src",
-      "no_parser",
-      // … и так далее
+      "table",                // 0
+      "79.137.204.8:9117",     // 1
+      "Ошибка",               // 2
+      "Error:",               // 3
+      "Jacred.xyz",           // 4
+      "jacred_viewbox_dev",   // 5
+      "change",               // 6
+      "field",                // 7
+      "ff2121",               // 8
+      "src",                  // 9
+      "no_parser",            // 10
+      // … остальные строки
+      // Пример: вместо getString(0x1fc) вернём закрывающую скобку
+      ")",                    // допустим, индекс 30
+      // И так далее для всех используемых индексов (например, 0x223, 0x200, 0x1f7, 0x205, 0x226, 0x215, 0x26f, 0x258, 0x1ff, 0x1f1, 0x20a, 0x253, 0x204, 0x238, 0x229, 0x212, 0x270, 0x1f4, 0x211, 0x236, 0x24b и т.д.)
     ];
   }
 
@@ -54,22 +57,19 @@
     }
   })(getObfuscationArray, 0xd3c06);
 
+  // Переменная observer вынесена в область видимости основного блока
+  var observer = null;
+
   // Основной функционал
   (function () {
     "use strict";
 
-    // Повторно включаем режим TV
     Lampa.Platform.tv();
-
-    // Принудительная установка некоторых настроек платформы (например, использование парсера)
     Lampa.Settings.set("jackett_enabled", true);
-
-    // Определяем базовый URL в зависимости от протокола страницы
     var baseUrl = location.protocol === "https:" ? "https:" : "http:";
 
-    // Массив адресов серверов для парсеров
     var parserServers = [
-      getString(0x258),
+      getString(0x258),          // убедитесь, что этот индекс определён
       "79.137.204.8:9117",
       getString(0x1ff),
       getString(0x1f1),
@@ -80,7 +80,6 @@
       getString(0x204),
     ];
 
-    // Массив «отображаемых» названий для каждого парсера
     var parserDisplayNames = [
       getString(0x238),
       "ByLampa Jackett",
@@ -93,42 +92,33 @@
       getString(0x236),
     ];
 
-    /**
-     * Функция для тестирования сервера парсера.
-     * Для каждого индекса формируется URL, отправляется AJAX-запрос и обновляется интерфейс.
-     */
     function testParserServer(index) {
       setTimeout(function () {
         var statusMessage = "";
-        // Пример: для определённых серверов подставляем дополнительные параметры
         if (parserServers[index] === getString(0x253))
           statusMessage = getString(0x205);
         if (parserServers[index] === getString(0x226))
           statusMessage = getString(0x215);
 
         var nextChildIndex = index + 2;
-        // Для специального случая (jr.maxvol.pro) принудительно ставим HTTPS
         if (parserServers[index] === "jr.maxvol.pro")
           baseUrl = "https:";
         else baseUrl = getString(0x26f);
 
-        // Формирование селектора для обновления элемента UI
+        // Добавляем закрывающую скобку вручную, если getString(0x1fc) не определён
         var selector =
           "body > div.selectbox > div.selectbox__content.layer--height > div.selectbox__body.layer--wheight > div > div > div > div:nth-child(" +
           nextChildIndex +
-          getString(0x1fc);
+          ")" ; // заменили getString(0x1fc) на ")"
 
-        // Если нужный элемент не найден – выходим
         if ($(getString(0x223)).text() !== getString(0x200)) return;
 
-        // Формирование полного URL для запроса
         var testUrl = baseUrl + parserServers[index] + getString(0x1f7) + statusMessage;
         var xhr = new XMLHttpRequest();
         xhr.timeout = 3000;
         xhr.open("GET", testUrl, true);
         xhr.send();
 
-        // Обработка ошибок и таймаута
         xhr.onerror = xhr.ontimeout = function () {
           if ($(selector).text() === parserDisplayNames[index])
             $(selector)
@@ -136,7 +126,6 @@
               .css(getString(0x277), getString(0x22d));
         };
 
-        // Обработка успешного ответа
         xhr.onload = function () {
           if (xhr.status === 200) {
             if ($(selector).text() === parserDisplayNames[index])
@@ -159,16 +148,12 @@
       }, 1000);
     }
 
-    // Функция для последовательного тестирования всех серверов-парсеров
     function testAllParserServers() {
       for (var i = 0; i < parserServers.length; i++) {
         testParserServer(i);
       }
     }
 
-    /**
-     * Функция обновления настроек парсера на основании выбора пользователя.
-     */
     function updateParserSettings() {
       var parser = Lampa.Storage.get("parser_type");
       if (parser === "default") {
@@ -178,10 +163,9 @@
         Lampa.Storage.set("some_flag", false);
         Lampa.Storage.set("jackett_lang", "lg");
       }
-      // Дополнительные условия для других типов парсеров...
+      // Другие условия по необходимости...
     }
 
-    // Привязка обработчика к изменению настроек: при выборе нового парсера запускается обновление настроек и тестирование серверов
     Lampa.Settings.addComponent({
       component: "parser_selector",
       param: {
@@ -243,9 +227,6 @@
       Lampa.Storage.set("jackett_lang", "lg");
     }
 
-    /**
-     * Функция формирования списка парсеров.
-     */
     function buildParserList() {
       var currentComponent = Lampa.Activity.enabled().component;
       var parserList = [];
@@ -266,7 +247,7 @@
         jac_int: "healthy",
         jac_lang: "df",
       });
-      // Добавляются другие парсеры
+      // Другие парсеры добавляются аналогично
 
       _fetchParserStatus(parserList)
         .then(function (statusList) {
@@ -334,54 +315,4 @@
             resolve(parserItem);
           } else {
             parserItem.title =
-              '<span style="color:#ff2121;">✘  ' + title + "</span>";
-            resolve(parserItem);
-          }
-        };
-        xhr.onerror = xhr.ontimeout = function () {
-          parserItem.title = "Error: " + title;
-          resolve(parserItem);
-        };
-        xhr.send();
-      });
-    }
-
-    Lampa.Settings.on("parser_menu", function (event) {
-      if (event.type === "select") {
-        if (Lampa.Activity.current().component === "parser_menu")
-          openParserMenu();
-        else closeParserMenu();
-      }
-    });
-
-    function openParserMenu() {
-      var targetNode = document.body;
-      var config = { childList: true, subtree: true };
-      var observer = new MutationObserver(function (mutationsList) {
-        mutationsList.forEach(function (mutation) {
-          if (
-            $(".empty__title").length &&
-            Lampa.Settings.field("parser_torrent_type") === "jackett"
-          ) {
-            buildParserList();
-            closeParserMenu();
-          }
-        });
-      });
-      observer.observe(targetNode, config);
-    }
-
-    function closeParserMenu() {
-      if (observer) {
-        observer.disconnect();
-        observer = null;
-      }
-    }
-
-    // Блок инициализации Яндекс Метрики удалён
-
-    // Добавление HTML-контейнера для кнопки выбора парсера
-    var parserMenuHtml = getString(0x24b);
-    $("body").append(parserMenuHtml);
-  }());
-}());
+              '<span style="
