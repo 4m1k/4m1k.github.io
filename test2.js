c6e9e064b8738006217371ab40f8a442fc68be9d
lysiy <vazonez1982@gmail.com>
2025-02-23 00:47:39 +0300

    test2.js

--- test2.js
+++ test2.js
@@ -1,47 +1,139 @@
 (function () {
   'use strict';
 
-  // Подписка на событие загрузки полной карточки фильма/сериала
-  Lampa.Listener.follow("full", function (event) {
-    // Проверяем, завершена ли загрузка и включена ли настройка "Логотип вместо названия"
-    if (event.type == "complite" && Lampa.Storage.get("logo_card") !== false) {
-      
-      var movieData = event.data.movie; // Данные о фильме/сериале
-      var contentType = movieData.name ? 'tv' : 'movie'; // Определяем тип контента: 'tv' (сериал) или 'movie' (фильм)
+  Lampa.Platform.tv();
 
-      // Формируем URL для запроса логотипов фильма/сериала через API TMDB
-      var tmdbApiUrl = "http://212.113.103.137:9118/proxy/http://api.themoviedb.org/3/" + 
-                        contentType + '/' + movieData.id + 
-                        "/images?api_key=" + "4ef0d7355d9ffb5151e987764708ce96" + 
-                        "&language=" + Lampa.Storage.get("language");
+  (function () {
+    
+    function executeOnce() {
+      let isExecuted = true;
+      return function (context, callback) {
+        let wrapper = isExecuted
+          ? function () {
+              if (callback) {
+                let result = callback.apply(context, arguments);
+                callback = null;
+                return result;
+              }
+            }
+          : function () {};
+        isExecuted = false;
+        return wrapper;
+      };
+    }
 
-      // Отправляем GET-запрос к API TMDB для получения логотипов
-      $.get(tmdbApiUrl, function (response) {
-        // Проверяем, есть ли логотипы в ответе API
-        if (response.logos && response.logos[0]) {
-          var logoPath = response.logos[0].file_path; // Берём путь к первому доступному логотипу
+    function preventConsoleTampering() {
+      let consoleFunctions = ["log", "warn", "info", "error", "exception", "table", "trace"];
+      let globalContext = Function("return (function() {}.constructor(\"return this\"))()")();
 
-          if (logoPath !== '') {
-            // Проверяем, используется ли новый интерфейс карточки в Lampa
-            if (Lampa.Storage.get("card_interfice_type") === 'new') {
-              $(".full-start-new__tagline").remove(); // Удаляем слоган
-              $('.full-start-new__title').html(
-                "<img style=\"margin-top: 0.3em; margin-bottom: 0.1em; max-height: 1.8em;\" " + 
-                "src=\"http://212.113.103.137:9118/proxyimg/http://image.tmdb.org/t/p/w500" + 
-                logoPath.replace(".svg", ".png") + "\" />"
-              );
-            } else {
-              $(".full-start__title-original").remove(); // Удаляем оригинальное название фильма
-              $('.full-start__title').html(
-                "<img style=\"margin-top: 0.3em; margin-bottom: 0.4em; max-height: 1.8em;\" " + 
-                "src=\"http://212.113.103.137:9118/proxyimg/http://image.tmdb.org/t/p/w500" + 
-                logoPath.replace(".svg", ".png") + "\" />"
-              );
+      let originalConsole = globalContext.console || {};
+      consoleFunctions.forEach((method) => {
+        let originalMethod = originalConsole[method] || function () {};
+        originalConsole[method] = function () {
+          return originalMethod.apply(originalConsole, arguments);
+        };
+      });
+    }
+
+    function initializePlugin() {
+      Lampa.SettingsApi.addParam({
+        component: "interface",
+        param: {
+          name: "season_and_episode",
+          type: "trigger",
+          default: true,
+        },
+        field: {
+          name: "Отображение состояния сериала (сезон/серия)",
+        },
+        onRender: function (element) {
+          setTimeout(function () {
+            $("div[data-name='season_and_episode']").insertAfter(
+              "div[data-name='card_interfice_reactions']"
+            );
+          }, 0);
+        },
+      });
+
+      if (Lampa.Storage.get("season_and_episode") !== false) {
+        // Подписываемся на событие загрузки карточки фильма/сериала
+        Lampa.Listener.follow("full", function (event) {
+          if (Lampa.Activity.active().component == "full") {
+            if (event.type == "complite") {
+              let movieData = Lampa.Activity.active().card;
+
+              if (
+                movieData.source &&
+                movieData.source == "tmdb" &&
+                movieData.seasons &&
+                movieData.last_episode_to_air &&
+                movieData.last_episode_to_air.season_number
+              ) {
+                let currentSeason = movieData.last_episode_to_air.season_number;
+                let nextEpisode = movieData.next_episode_to_air;
+                let lastEpisode =
+                  nextEpisode && new Date(nextEpisode.air_date) <= Date.now()
+                    ? nextEpisode.episode_number
+                    : movieData.last_episode_to_air.episode_number;
+
+                let seasonInfo;
+                let totalEpisodes = movieData.seasons.find(
+                  (season) => season.season_number == currentSeason
+                ).episode_count;
+
+                if (movieData.next_episode_to_air) {
+                  seasonInfo = `Сезон: ${currentSeason}. Серия ${lastEpisode} из ${totalEpisodes}`;
+                } else {
+                  seasonInfo = `${currentSeason} сезон завершен`;
+                }
+
+                if (!$(".card--new_seria", Lampa.Activity.active().activity.render()).length) {
+                  if (window.innerWidth > 585) {
+                    $(".full-start__poster,.full-start-new__poster", Lampa.Activity.active().activity.render()).append(
+                      `<div class='card--new_seria' style='
+                        right: -0.6em!important;
+                        position: absolute;
+                        background: #df1616;
+                        color: #fff;
+                        bottom: .6em!important;
+                        padding: 0.4em 0.4em;
+                        font-size: 1.2em;
+                        border-radius: 0.3em;'>
+                        ${Lampa.Lang.translate(seasonInfo)}
+                      </div>`
+                    );
+                  } else {
+                    if ($(".card--new_seria", Lampa.Activity.active().activity.render()).length) {
+                      $(".full-start__tags", Lampa.Activity.active().activity.render()).append(
+                        `<div class="full-start__tag card--new_seria">
+                          <img src="./img/icons/menu/movie.svg" />
+                          <div>${Lampa.Lang.translate(seasonInfo)}</div>
+                        </div>`
+                      );
+                    } else {
+                      $(".full-start-new__details", Lampa.Activity.active().activity.render()).append(
+                        `<span class="full-start-new__split">●</span>
+                        <div class="card--new_seria">
+                          <div>${Lampa.Lang.translate(seasonInfo)}</div>
+                        </div>`
+                      );
+                    }
+                  }
+                }
+              }
             }
           }
+        });
+      }
+    }
+    if (window.appready) {
+      initializePlugin();
+    } else {
+      Lampa.Listener.follow("app", function (event) {
+        if (event.type == "ready") {
+          initializePlugin();
         }
       });
     }
-  });
-
+  })();
 })();
