bec5e1e8172302e615f792700b6c68243a061a65
lysiy <vazonez1982@gmail.com>
2025-02-26 00:09:28 +0300

    test2.js

--- test2.js
+++ test2.js
@@ -817,3 +817,185 @@
   })();
 
 })();
+
+/***************************************************************************
+ * Часть 4. Создаём компонент в настройках Lampa для пунктов «Кинопоиск»
+ **************************************************************************/
+(function addKpSettingsComponent() {
+  // 1) Регистрируем новый компонент настроек
+  Lampa.SettingsApi.addComponent({
+    component: 'kp_plugin_menu',           // уникальное имя компонента
+    name: 'Категории Кинопоиска',          // как будет называться в списке
+    // Можем использовать ту же иконку, что и у кнопки «Кинопоиск»
+    // или вставить свою:
+    icon: `
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="192"
+        height="192"
+        viewBox="0 0 192 192"
+      >
+        <g fill="none" fill-rule="evenodd">
+          <g fill="currentColor" fill-rule="nonzero">
+            <path
+              fill-rule="evenodd"
+              d="
+                M20,4
+                H172
+                A16,16 0 0 1 188,20
+                V172
+                A16,16 0 0 1 172,188
+                H20
+                A16,16 0 0 1 4,172
+                V20
+                A16,16 0 0 1 20,4
+                Z
+
+                M20,18
+                H172
+                A2,2 0 0 1 174,20
+                V172
+                A2,2 0 0 1 172,174
+                H20
+                A2,2 0 0 1 18,172
+                V20
+                A2,2 0 0 1 20,18
+                Z
+              "
+            />
+            <g transform="translate(-10.63, 0)">
+              <path
+                d="
+                  M96.5 20
+                  L66.1 75.733
+                  V20
+                  H40.767
+                  v152
+                  H66.1
+                  v-55.733
+                  L96.5 172
+                  h35.467
+                  C116.767 153.422 95.2 133.578 80 115
+                  c28.711 16.889 63.789 35.044 92.5 51.933
+                  v-30.4
+                  C148.856 126.4 108.644 115.133 85 105
+                  c23.644 3.378 63.856 7.889 87.5 11.267
+                  v-30.4
+                  L85 90
+                  c27.022-11.822 60.478-22.711 87.5-34.533
+                  v-30.4
+                  C143.789 41.956 108.711 63.11 80 80
+                  L131.967 20
+                  z
+                "
+              />
+            </g>
+          </g>
+        </g>
+      </svg>
+    `
+  });
+
+  // Функция-хелпер для скрытия "значений" (чтобы не было выпадающих списков)
+  function hideValue(item) {
+    // скрываем .settings-param__value, если хотим статический пункт
+    $('.settings-param__value', item).hide();
+  }
+
+  // 2) Добавляем пункты как статические, каждый — через addParam
+  // Пример для «Популярные фильмы»
+  Lampa.SettingsApi.addParam({
+    component: 'kp_plugin_menu',
+    param: {
+      name: 'kp_popular_films',
+      type: 'static', // статический пункт
+      default: true
+    },
+    field: {
+      // HTML-код с нужной иконкой и классом `menu__text`
+      name: `
+        <div class="settings-folder" style="padding:0!important; display: flex; align-items: center;">
+          <!-- иконка -->
+          <div style="width:1.8em; height:1.3em; padding-right:.5em;">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <g fill="none">
+                <path stroke="currentColor" stroke-width="1.5"
+                  d="M22 16c0 2.828 0 4.243-.879 5.121C20.243 22 18.828 22 16 22H8c-2.828 0-4.243 0-5.121-.879C2 20.243 2 18.828 2 16v-4c0-2.828 0-4.243.879-5.121C3.757 6 5.172 6 8 6h8c2.828 0 4.243 0 5.121.879C22 7.757 22 9.172 22 12z"/>
+                <path stroke="currentColor" stroke-linecap="round" stroke-width="1.5"
+                  d="m9 2l3 3.5L15 2m1 4v16"/>
+                <path fill="currentColor"
+                  d="M20 16a1 1 0 1 0-2 0a1 1 0 0 0 2 0m0-4a1 1 0 1 0-2 0a1 1 0 0 0 2 0"/>
+              </g>
+            </svg>
+          </div>
+          <!-- Текст со шрифтом Lampa -->
+          <div class="menu__text">Популярные фильмы</div>
+        </div>
+      `,
+      description: 'Пример пункта «Популярные фильмы»'
+    },
+    onRender: function (item) {
+      hideValue(item);
+      // При клике «enter» можно сделать переход
+      item.on('hover:enter', function () {
+        // Например, сразу пушим Activity
+        Lampa.Activity.push({
+          url: 'api/v2.2/films/top?type=TOP_100_POPULAR_FILMS',
+          title: 'Популярные Фильмы',
+          component: 'category_full',
+          source: 'KP',
+          card_type: true,
+          page: 1
+        });
+      });
+    }
+  });
+
+  // 3) Аналогично добавляем пункт «Топ Фильмы»
+  Lampa.SettingsApi.addParam({
+    component: 'kp_plugin_menu',
+    param: {
+      name: 'kp_top_films',
+      type: 'static',
+      default: true
+    },
+    field: {
+      name: `
+        <div class="settings-folder" style="padding:0!important; display: flex; align-items:center;">
+          <div style="width:1.8em;height:1.3em;padding-right:.5em">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+              <g fill="none">
+                <path stroke="currentColor" stroke-width="1.5"
+                  d="M22 16c0 2.828 0 4.243-.879 5.121C20.243 22 18.828 22 16 22H8c-2.828 0-4.243 0-5.121-.879C2 20.243 2 18.828 2 16v-4c0-2.828 0-4.243.879-5.121C3.757 6 5.172 6 8 6h8c2.828 0 4.243 0 5.121.879C22 7.757 22 9.172 22 12z"/>
+                <path stroke="currentColor" stroke-linecap="round" stroke-width="1.5"
+                  d="m9 2l3 3.5L15 2m1 4v16"/>
+                <path fill="currentColor"
+                  d="M20 16a1 1 0 1 0-2 0a1 1 0 0 0 2 0m0-4a1 1 0 1 0-2 0a1 1 0 0 0 2 0"/>
+              </g>
+            </svg>
+          </div>
+          <div class="menu__text">Топ Фильмы</div>
+        </div>
+      `,
+      description: ''
+    },
+    onRender: function (item) {
+      hideValue(item);
+      item.on('hover:enter', function () {
+        Lampa.Activity.push({
+          url: 'api/v2.2/films/top?type=TOP_250_BEST_FILMS',
+          title: 'Топ Фильмы',
+          component: 'category_full',
+          source: 'KP',
+          card_type: true,
+          page: 1
+        });
+      });
+    }
+  });
+
+  // 4) По аналогии добавьте остальные пункты: «Популярные российские фильмы», «Популярные сериалы» и т.д.
+  //    С разными url: 'api/v2.2/films?order=NUM_VOTE&countries=34&type=FILM' и т.п.
+
+})();
+
