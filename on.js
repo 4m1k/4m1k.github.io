4373e5b142e4080fbc27de134ac7d1ed63ee5023
lysiy <vazonez1982@gmail.com>
2025-03-02 00:59:24 +0300

    on.js

--- on.js
+++ on.js
@@ -286,6 +286,9 @@
       if (balanser && balanser.toLowerCase() === 'filmixtv') {
         url = "http://rc.bwa.to/rc/fxapi";
       }
+	else if (balanser && balanser.toLowerCase() === 'fancdn') {
+         url = "http://vcdn3.skaz.tv/lite/fancdn";
+     }
       var query = [];
       var card_source = object.movie.source || 'tmdb'; 
       query.push('id=' + object.movie.id);
