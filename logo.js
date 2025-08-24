0c2d35c4acad08b58c5055035103683bcf90c0ba
lysiy <vazonez1982@gmail.com>
2025-08-24 11:39:17 +0300

    logo.js

--- logo.js
+++ logo.js
@@ -5,7 +5,7 @@
     if (event.type == "complite" && Lampa.Storage.get("logo_card") !== false) {
       var movieData = event.data.movie; 
       var contentType = movieData.name ? 'tv' : 'movie'; 
-      var tmdbApiUrl = "http://212.113.103.137:9118/proxy/http://api.themoviedb.org/3/" + 
+      var tmdbApiUrl = "https://lampa.maxvol.pro/proxy/http://api.themoviedb.org/3/" + 
                         contentType + '/' + movieData.id + 
                         "/images?api_key=" + "4ef0d7355d9ffb5151e987764708ce96" + 
                         "&language=" + Lampa.Storage.get("language");
@@ -19,14 +19,14 @@
               $(".full-start-new__tagline").remove();
               $('.full-start-new__title').html(
                 "<img style=\"margin-top: 0.3em; margin-bottom: 0.1em; max-height: 1.8em;\" " + 
-                "src=\"http://212.113.103.137:9118/proxyimg/http://image.tmdb.org/t/p/w500" + 
+                "src=\"https://lampa.maxvol.pro/proxyimg/http://image.tmdb.org/t/p/w500" + 
                 logoPath.replace(".svg", ".png") + "\" />"
               );
             } else {
               $(".full-start__title-original").remove();
               $('.full-start__title').html(
                 "<img style=\"margin-top: 0.3em; margin-bottom: 0.4em; max-height: 1.8em;\" " + 
-                "src=\"http://212.113.103.137:9118/proxyimg/http://image.tmdb.org/t/p/w500" + 
+                "src=\"https://lampa.maxvol.pro/proxyimg/http://image.tmdb.org/t/p/w500" + 
                 logoPath.replace(".svg", ".png") + "\" />"
               );
             }
