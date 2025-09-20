9ffaa66e0d85079526a768fb0111e0a135ee8660
lysiy <vazonez1982@gmail.com>
2025-09-20 19:28:38 +0300

    o.js

--- o.js
+++ o.js
@@ -16,16 +16,11 @@ var randomUrl = vybor[randomIndex];
 
   var balansers_with_search;
   
-var unic_id = Lampa.Storage.get('lampac_unic_id', '');
-if (!unic_id) {
-  unic_id = 'ofkrtxdg';
-  Lampa.Storage.set('lampac_unic_id', unic_id);
-}
-
-if (!Lampa.Storage.get('account_email', '')) {
-  Lampa.Storage.set('account_email', 'lisiyvirus@gmail.com');
-}
-
+  var unic_id = Lampa.Storage.get('lampac_unic_id', '');
+  if (!unic_id) {
+    unic_id = Lampa.Utils.uid(8).toLowerCase();
+    Lampa.Storage.set('lampac_unic_id', unic_id);
+  }
   
   var hostkey = 'http://online3.skaz.tv'.replace('http://', '').replace('https://', '');
 
@@ -1564,54 +1559,7 @@ else if (element.url) {
     window.onlyskaz_plugin = true;
 	if (!window.plugin_iptvskaz_ready && !window.plugin_iptv_ready2)
 	  {
-		Lampa.SettingsApi.addComponent({
-        component: 'iptvskaz',
-        icon: "<svg height=\"36\" viewBox=\"0 0 38 36\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\">\n                <rect x=\"2\" y=\"8\" width=\"34\" height=\"21\" rx=\"3\" stroke=\"white\" stroke-width=\"3\"/>\n                <line x1=\"13.0925\" y1=\"2.34874\" x2=\"16.3487\" y2=\"6.90754\" stroke=\"white\" stroke-width=\"3\" stroke-linecap=\"round\"/>\n                <line x1=\"1.5\" y1=\"-1.5\" x2=\"9.31665\" y2=\"-1.5\" transform=\"matrix(-0.757816 0.652468 0.652468 0.757816 26.197 2)\" stroke=\"white\" stroke-width=\"3\" stroke-linecap=\"round\"/>\n                <line x1=\"9.5\" y1=\"34.5\" x2=\"29.5\" y2=\"34.5\" stroke=\"white\" stroke-width=\"3\" stroke-linecap=\"round\"/>\n            </svg>",
-        name: 'by skaz'
-      });
-	  Lampa.SettingsApi.addParam({
-				component: 'iptvskaz',
-				param: {
-					name: 'only_title',
-					type: 'title',
-					default: true
-				},
-				field: {
-					name: 'Онлайн'
-				}
-			});
-	  	  Lampa.SettingsApi.addParam({
-        component: 'iptvskaz',
-        param: {
-          name: 'online_skaz2',
-          type: 'select',
-           values: {
-                'true': 'Отображать',
-                'false': 'Скрыть',
-            },
-            default: false,
-        },
-        field: {
-          name: 'Отображать Onlyskaz 2.0',
-		  description: 'Онлайн с автовыбором источника, управление в плеере'
-        }
-	  });
-	  	  	  Lampa.SettingsApi.addParam({
-        component: 'iptvskaz',
-        param: {
-          name: 'skazonline2_view',
-          type: 'trigger',
-            default: false,
-        },
-        field: {
-          name: 'Спрятать значок Onlyskaz 2.0',
-		  description: 'Позволяет скрыть значок с карточки фильма'
-        },
-        onChange: function (value) {
-			Lampa.Noty.show('Необходимо перезайти в лампу');
 		}
-	  });
-	}
     var manifst = {
       type: 'video',
       version: '',
