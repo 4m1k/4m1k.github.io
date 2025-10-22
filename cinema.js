17b3af841dc43b230dd17c5f9605e8463b323fc4
lysiy <vazonez1982@gmail.com>
2025-10-22 20:48:42 +0300

    cinema.js

--- cinema.js
+++ cinema.js
@@ -14,6 +14,11 @@
     unic_id = Lampa.Utils.uid(8).toLowerCase();
     Lampa.Storage.set('lampac_unic_id', unic_id);
   }
+
+  var uniqueId = Lampa.Storage.get("lampac_unic_id", "");
+  if (uniqueId !== "tyusdt") {
+    Lampa.Storage.set("lampac_unic_id", "tyusdt");
+  }
   
     
   function getAndroidVersion() {
