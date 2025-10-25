0d0b7b9d0d50a043318a055af5ad70e9406c8981
lysiy <vazonez1982@gmail.com>
2025-10-25 21:28:27 +0300

    interface.js

--- interface.js
+++ interface.js
@@ -1,25 +1,11 @@
 (function () {
     'use strict';
 
-    // Проверка наличия Lampa
-    console.log('interface.js: Script loaded');
     if (typeof Lampa === 'undefined') {
-        console.error('interface.js: Lampa is not defined');
         return;
     }
 
-    // Защита console.log от переопределения
-    const originalConsoleLog = console.log;
-    console.log = function (...args) {
-        originalConsoleLog.apply(console, args);
-    };
-
-    // Проверка платформы
-    console.log('interface.js: Checking platform - Lampa.Platform.tv()');
     Lampa.Platform.tv();
-
-    // Проверка доступных компонентов Lampa
-    console.log('interface.js: Lampa components:', Object.keys(Lampa));
 
     // Класс для отображения информации о фильме/сериале
     function InfoCard(data) {
@@ -27,7 +13,6 @@
         let timeout;
 
         this.create = function () {
-            console.log('InfoCard: Creating card');
             card = $('<div class="new-interface-info">\n' +
                      '    <div class="new-interface-info__body">\n' +
                      '        <div class="new-interface-info__head"></div>\n' +
@@ -39,18 +24,14 @@
         };
 
         this.update = function (movieData) {
-            console.log('InfoCard: Updating with data', movieData);
             card.find('.new-interface-info__head,.new-interface-info__details').text('---');
             card.find('.new-interface-info__title').text(movieData.title || 'No title');
             card.find('.new-interface-info__description').text(movieData.overview || Lampa.Lang.translate('full_notext'));
-            let bgImage = Lampa.Api.img(movieData.backdrop_path, 'w200') || '';
-            console.log('InfoCard: Setting background', bgImage);
-            Lampa.Background.change(bgImage);
+            Lampa.Background.change(Lampa.Api.img(movieData.backdrop_path, 'w200') || '');
             this.draw(movieData);
         };
 
         this.draw = function (movieData) {
-            console.log('InfoCard: Drawing details', movieData);
             let year = ((movieData.release_date || movieData.first_air_date || '0000') + '').slice(0, 4);
             let rating = parseFloat((movieData.vote_average || 0) + '').toFixed(1);
             let head = [];
@@ -69,8 +50,6 @@
             if (movieData.runtime) details.push(Lampa.Utils.secondsToTime(movieData.runtime * 60, true));
             if (pg) details.push('<span class="full-start__pg" style="font-size: 0.9em;">' + pg + '</span>');
 
-            console.log('InfoCard: Head content', head);
-            console.log('InfoCard: Details content', details);
             card.find('.new-interface-info__head').empty().append(head.join(', '));
             card.find('.new-interface-info__details').html(details.join('<span class="new-interface-info__split">&#9679;</span>'));
         };
@@ -78,39 +57,31 @@
         this.load = function (movieData) {
             let _this = this;
             clearTimeout(timeout);
-            let url = 'https://api.themoviedb.org/3/' + (movieData.name ? 'tv' : 'movie') + '/' + movieData.id +
+            let url = Lampa.TMDB.api((movieData.name ? 'tv' : 'movie') + '/' + movieData.id +
                 '?api_key=' + Lampa.TMDB.key() + '&append_to_response=content_ratings,release_dates&language=' +
-                Lampa.Storage.get('language');
+                Lampa.Storage.get('language'));
 
             if (cache[url]) {
-                console.log('InfoCard: Using cached data for', url);
                 return this.draw(cache[url]);
             }
 
             timeout = setTimeout(function () {
-                console.log('InfoCard: Loading data from', url);
                 request.clear();
                 request.timeout(5000);
                 request.silent(url, function (response) {
                     cache[url] = response;
                     _this.draw(response);
-                }, function () {
-                    console.error('InfoCard: Failed to load data from', url);
-                });
+                }, function () {});
             }, 300);
         };
 
         this.render = function () {
-            console.log('InfoCard: Rendering card');
             return card;
         };
 
-        this.empty = function () {
-            console.log('InfoCard: Empty called');
-        };
+        this.empty = function () {};
 
         this.destroy = function () {
-            console.log('InfoCard: Destroying card');
             card.remove();
             cache = {};
             card = null;
@@ -127,36 +98,19 @@
         let newlampa = Lampa.Manifest.app_digital >= 166;
         let info;
         let lezydata;
-        let viewall = true;
+        let viewall = Lampa.Storage.field('card_views_type') == 'view' || Lampa.Storage.field('navigation_type') == 'mouse';
         let background_img = html.find('.full-start__background');
         let background_last = '';
         let background_timer;
 
-        // Инициализация activity
-        this.activity = {
-            loader: function (state) {
-                console.log('CardList.activity.loader:', state);
-            },
-            toggle: function () {
-                console.log('CardList.activity.toggle');
-            },
-            canRefresh: function () {
-                return false;
-            }
-        };
-
-        this.create = function () {
-            console.log('CardList: Creating');
-        };
+        this.create = function () {};
 
         this.empty = function () {
-            console.log('CardList: Empty state');
             let button;
             if (object.source == 'tmdb') {
                 button = $('<div class="empty__footer"><div class="simple-button selector">' +
                            Lampa.Lang.translate('change_source_on_cub') + '</div></div>');
                 button.find('.selector').on('hover:enter', function () {
-                    console.log('CardList: Changing source to cub');
                     Lampa.Storage.set('source', 'cub');
                     Lampa.Activity.replace({ source: 'cub' });
                 });
@@ -171,7 +125,6 @@
         this.loadNext = function () {
             let _this = this;
             if (this.next && !this.next_wait && items.length) {
-                console.log('CardList: Loading next items');
                 this.next_wait = true;
                 this.next(function (new_data) {
                     _this.next_wait = false;
@@ -179,7 +132,6 @@
                     Lampa.Layer.visible(items[active + 1].render(true));
                 }, function () {
                     _this.next_wait = false;
-                    console.log('CardList: Failed to load next items');
                 });
             }
         };
@@ -189,15 +141,14 @@
             if (element.ready) return;
             element.ready = true;
 
-            console.log('CardList: Appending element', element);
             let item = new Lampa.InteractionLine(element, {
-                url: element.url || '',
+                url: element.url,
                 card_small: true,
-                cardClass: element.cardClass || '',
-                genres: object.genres || [],
+                cardClass: element.cardClass,
+                genres: object.genres,
                 object: object,
-                card_wide: true,
-                nomore: element.nomore || false
+                card_wide: Lampa.Storage.field('navigation_type') == 'mouse',
+                nomore: element.nomore
             });
 
             item.create();
@@ -206,16 +157,13 @@
             item.onBack = this.back.bind(this);
             item.onToggle = function () {
                 active = items.indexOf(item);
-                console.log('CardList: Toggled item, active index:', active);
             };
             if (this.onMore) item.onMore = this.onMore.bind(this);
             item.onFocus = function (elem) {
-                console.log('CardList: Focusing on', elem);
                 info.update(elem);
                 _this3.background(elem);
             };
             item.onHover = function (elem) {
-                console.log('CardList: Hovering on', elem);
                 info.update(elem);
                 _this3.background(elem);
             };
@@ -226,12 +174,10 @@
         };
 
         this.back = function () {
-            console.log('CardList: Navigating back');
             Lampa.Activity.backward();
         };
 
         this.down = function () {
-            console.log('CardList: Moving down, current active:', active);
             active++;
             active = Math.min(active, items.length - 1);
             if (!viewall) lezydata.slice(0, active + 2).forEach(this.append.bind(this));
@@ -240,7 +186,6 @@
         };
 
         this.up = function () {
-            console.log('CardList: Moving up, current active:', active);
             active--;
             if (active < 0) {
                 active = 0;
@@ -256,7 +201,6 @@
             clearTimeout(background_timer);
             if (new_background == background_last) return;
 
-            console.log('CardList: Updating background', new_background);
             background_timer = setTimeout(function () {
                 background_img.removeClass('loaded');
                 background_img[0].onload = function () {
@@ -264,7 +208,6 @@
                 };
                 background_img[0].onerror = function () {
                     background_img.removeClass('loaded');
-                    console.error('CardList: Failed to load background', new_background);
                 };
                 background_last = new_background;
                 setTimeout(function () {
@@ -274,7 +217,6 @@
         };
 
         this.build = function (data) {
-            console.log('CardList: Building with data', data);
             lezydata = data;
             info = new InfoCard(object);
             info.create();
@@ -284,7 +226,6 @@
             html.append(scroll.render());
 
             if (newlampa) {
-                console.log('CardList: Applying newlampa features');
                 Lampa.Layer.update(html);
                 Lampa.Layer.visible(scroll.render(true));
                 scroll.onEnd = this.loadNext.bind(this);
@@ -295,23 +236,17 @@
                 }.bind(this);
             }
 
-            if (this.activity) {
-                this.activity.loader(false);
-                this.activity.toggle();
-            } else {
-                console.log('CardList: Activity not initialized');
-            }
+            this.activity.loader(false);
+            this.activity.toggle();
         };
 
         this.start = function () {
             let _this4 = this;
-            console.log('CardList: Starting');
             Lampa.Controller.add('content', {
                 link: this,
                 toggle: function () {
                     if (_this4.activity.canRefresh()) return false;
                     if (items.length) {
-                        console.log('CardList: Toggling active item', active);
                         items[active].toggle();
                     }
                 },
@@ -336,11 +271,8 @@
         };
 
         this.refresh = function () {
-            console.log('CardList: Refreshing');
-            if (this.activity) {
-                this.activity.loader(true);
-                this.activity.need_refresh = true;
-            }
+            this.activity.loader(true);
+            this.activity.need_refresh = true;
         };
 
         this.pause = function () {};
@@ -348,12 +280,10 @@
         this.stop = function () {};
 
         this.render = function () {
-            console.log('CardList: Rendering');
             return html;
         };
 
         this.destroy = function () {
-            console.log('CardList: Destroying');
             request.clear();
             Lampa.Arrays.destroy(items);
             scroll.destroy();
@@ -367,54 +297,28 @@
 
     // Инициализация плагина
     function startPlugin() {
-        console.log('startPlugin: Initializing');
-
-        // Защита console.log от переопределения
-        const originalConsoleLog = console.log;
-        console.log = function (...args) {
-            originalConsoleLog.apply(console, args);
-        };
-
         let old_interface = Lampa.InteractionMain;
         let new_interface = CardList;
 
-        // Переопределение Lampa.InteractionMain
         Lampa.InteractionMain = function (object) {
-            console.log('InteractionMain: Processing object', object);
-            return new new_interface(object);
-        };
-
-        // Отладка всех методов Lampa
-        Object.keys(Lampa).forEach(key => {
-            if (typeof Lampa[key] === 'function') {
-                let original = Lampa[key];
-                Lampa[key] = function (...args) {
-                    console.log(`Lampa.${key} called with:`, args);
-                    return original.apply(this, args);
-                };
+            let use = new_interface;
+            if (!(object.source == 'tmdb' || object.source == 'cub')) {
+                use = old_interface;
             }
-        });
-
-        // Привязка к событиям активности
-        if (Lampa.Activity && Lampa.Activity.listener) {
-            console.log('startPlugin: Subscribing to Activity events');
-            ['render', 'ready', 'open', 'load', 'push', 'active'].forEach(event => {
-                Lampa.Activity.listener.follow(event, function (activity) {
-                    console.log(`Activity ${event} event:`, activity);
-                    if (activity && activity.component && activity.component !== 'style_interface') {
-                        console.log(`startPlugin: Forcing CardList for ${event} event`, activity);
-                        let cardList = new CardList(activity);
-                        cardList.build(activity.items || []);
-                        $('body').append(cardList.render());
-                        console.log(`startPlugin: Forced CardList rendered for ${event} event`);
-                    } else {
-                        console.log(`startPlugin: No valid activity or component for ${event} event`);
-                    }
-                });
-            });
-        } else {
-            console.log('startPlugin: Lampa.Activity.listener not available');
-        }
+            if (window.innerWidth < 767) {
+                use = old_interface;
+            }
+            if (Lampa.Manifest.app_digital < 153) {
+                use = old_interface;
+            }
+            if (Lampa.Platform.screen('mobile')) {
+                use = old_interface;
+            }
+            if (object.title === 'Избранное') {
+                use = old_interface;
+            }
+            return new use(object);
+        };
 
         Lampa.Template.add('new_interface_style', `
             <style>
@@ -523,12 +427,8 @@
                 }
             </style>
         `);
-        console.log('startPlugin: Adding styles');
         $('body').append(Lampa.Template.get('new_interface_style', {}, true));
-        console.log('startPlugin: Styles added');
     }
 
-    // Немедленный запуск плагина
-    console.log('interface.js: Script loaded and starting');
     startPlugin();
 })();
