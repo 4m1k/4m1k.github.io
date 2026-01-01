aff9666d4ff0f3c9caca1ecb19994f12d929fe07
lysiy <vazonez1982@gmail.com>
2026-01-01 21:19:18 +0300

    logo.js

--- logo.js
+++ logo.js
@@ -1,31 +1,81 @@
 (function () {
     'use strict';
+
     // Функция для запуска плагина
     function startPlugin() {
         // Устанавливаем флаг, что плагин активирован
         window.logoplugin = true;
+
         // Подписываемся на событие 'full' в Lampa.Listener
         Lampa.Listener.follow('full', function (e) {
-            // Проверяем, что событие имеет тип 'complite' и параметр logo_glav не равен '1'
+            // Проверяем, что событие имеет тип 'complite' и логотипы включены
             if (e.type === 'complite' && Lampa.Storage.get('logo_glav') !== '1') {
                 var data = e.data.movie;
                 var type = data.name ? 'tv' : 'movie';
-                // Проверяем, что ID фильма не пустое
+
+                // Проверяем, что ID не пустое
                 if (data.id !== '') {
-                    // Формируем URL для запроса к API TMDB
-                    var url = Lampa.TMDB.api(type + '/' + data.id + '/images?api_key=' + Lampa.TMDB.key() + '&language=' + Lampa.Storage.get('language'));
+                    var lang = Lampa.Storage.get('language');
+
+                    // Формируем URL с приоритетом языка логотипа
+                    var url = Lampa.TMDB.api(
+                        type + '/' + data.id + '/images?api_key=' + Lampa.TMDB.key() +
+                        '&language=' + lang +
+                        '&include_image_language=' + lang + ',en,null'
+                    );
+
                     // Выполняем GET-запрос к API
-                    $.get(url, function (data) {
-                        // Проверяем наличие логотипов в ответе
-                        if (data.logos && data.logos[0]) {
-                            var logo = data.logos[0].file_path;
-                            // Если логотип существует, заменяем текстовое название на изображение
-                            if (logo !== '') {
-                                var renderElement = e.object.activity.render();
-                                renderElement.find('.full-start-new__title').html('<img style="margin-top:5px; max-height:125px;" src="' + Lampa.TMDB.image('/t/p/w500' + logo.replace('.svg', '.png')) + '"/>');
-                                
-                                // Удаляем элемент с теглайном для лучшей подгонки логотипа
-                                renderElement.find('.full-start-new__tagline').remove();
+                    $.get(url, function (response) {
+                        var logo_path = null;
+
+                        if (response.logos && response.logos.length > 0) {
+                            // Сначала ищем логотип на языке приложения
+                            for (var i = 0; i < response.logos.length; i++) {
+                                if (response.logos[i].iso_639_1 === lang) {
+                                    logo_path = response.logos[i].file_path;
+                                    break;
+                                }
+                            }
+                            // Если не нашли — английский
+                            if (!logo_path) {
+                                for (var i = 0; i < response.logos.length; i++) {
+                                    if (response.logos[i].iso_639_1 === 'en') {
+                                        logo_path = response.logos[i].file_path;
+                                        break;
+                                    }
+                                }
+                            }
+                            // Если и его нет — берём первый доступный
+                            if (!logo_path) {
+                                logo_path = response.logos[0].file_path;
+                            }
+                        }
+
+                        // Если логотип найден — заменяем название
+                        if (logo_path) {
+                            var renderElement = e.object.activity.render();
+
+                            renderElement.find('.full-start-new__title').html(
+                                '<img style="margin-top:5px; max-height:135px; display:block; margin-left:auto; margin-right:auto;" ' +
+                                'src="' + Lampa.TMDB.image('/t/p/original' + logo_path.replace('.svg', '.png')) + '"/>'
+                            );
+
+                            // Перенос года и страны под логотип (если включена опция)
+                            if (Lampa.Storage.get('logo_hide_year', true)) {
+                                var head = renderElement.find('.full-start-new__head');
+                                var details = renderElement.find('.full-start-new__details');
+
+                                if (head.length && details.length && details.find('.logo-moved-head').length === 0) {
+                                    var headContent = head.html().trim();
+                                    if (headContent) {
+                                        var separator = details.children().length > 0
+                                            ? '<span class="full-start-new__split logo-moved-separator">●</span>'
+                                            : '';
+                                        var movedHead = '<span class="logo-moved-head" style="margin-left:0.6em;">' + headContent + '</span>';
+                                        details.append(separator + movedHead);
+                                        head.remove();
+                                    }
+                                }
                             }
                         }
                     });
@@ -33,16 +83,14 @@
             }
         });
     }
-    // Добавляем параметр в настройки Lampa
+
+    // Добавляем параметры в настройки Lampa
     Lampa.SettingsApi.addParam({
         component: 'interface',
         param: {
             name: 'logo_glav',
             type: 'select',
-            values: {
-                '1': 'Скрыть',
-                '0': 'Отображать'
-            },
+            values: { '1': 'Скрыть', '0': 'Отображать' },
             default: '0'
         },
         field: {
@@ -50,6 +98,20 @@
             description: 'Отображает логотипы фильмов вместо текста'
         }
     });
+
+    Lampa.SettingsApi.addParam({
+        component: 'interface',
+        param: {
+            name: 'logo_hide_year',
+            type: 'trigger',
+            default: true
+        },
+        field: {
+            name: 'Скрывать год и страну над логотипом',
+            description: 'Переносит год выпуска и страну под логотип'
+        }
+    });
+
     // Запускаем плагин, если он еще не активирован
     if (!window.logoplugin) {
         startPlugin();
