7d78dbbc62113d00ad3b52cea0c39142a715b60b
lysiy <vazonez1982@gmail.com>
2026-01-01 19:41:54 +0300

    start.js

--- start.js
+++ start.js
@@ -1,15 +1,12 @@
 (function () {
     'use strict';
-
     Lampa.Platform.tv();
-
     var plugin = {
         name: 'TMDB Proxy with Anti-DMCA',
         version: '1.0.3',
         description: 'Проксирование постеров и API сайта TMDB с отключением DMCA-фич и обходом блокировок'
     };
-
-    plugin.path_image = Lampa.Utils.protocol() + 'tmdbimage.abmsx.tech/'; //tmdbimg.bylampa.online/  tmdbimage.abmsx.tech/
+    plugin.path_image = Lampa.Utils.protocol() + 'tmdbimage.abmsx.tech/';  //tmdbimg.bylampa.online/  tmdbimage.abmsx.tech/
     plugin.path_api = Lampa.Utils.protocol() + 'tmdb.abmsx.tech/3/'; //tmdbapi.bylampa.online/3/   tmdb.abmsx.tech/3/
 
     Lampa.TMDB.image = function (url) {
@@ -22,16 +19,22 @@
         return Lampa.Storage.field('proxy_tmdb') ? plugin.path_api + url : base;
     };
 
+    // Добавленная часть
+    // for Lampa.
+    window.lampa_settings = window.lampa_settings || {};
+    window.lampa_settings.dcma = false;
+    window.lampa_settings.disable_features = window.lampa_settings.disable_features || {};
+    window.lampa_settings.disable_features.dmca = true;
+
     function start() {
         if (window.anti_dmca_plugin) {
             return;
         }
         window.anti_dmca_plugin = true;
-
-        Lampa.Utils.dcma = function () { return undefined; };
-
+        Lampa.Utils.dcma = function () {
+            return undefined;
+        };
         var defaultSource = Lampa.Storage.get('source', 'cub');
-
         Lampa.Listener.follow('request_secuses', function (event) {
             if (event.data.blocked) {
                 window.lampa_settings.dcma = [];
@@ -42,7 +45,6 @@
                 Lampa.Storage.set('source', defaultSource, true);
             }
         });
-
         Lampa.Settings.listener.follow('open', function (e) {
             if (e.name === 'tmdb') {
                 e.body.find('[data-parent="proxy"]').remove();
@@ -59,5 +61,4 @@
             }
         });
     }
-
 })();
