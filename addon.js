07e23e03685be6ff3b957b712a45d2735d1692d1
lysiy <vazonez1982@gmail.com>
2026-01-05 18:52:48 +0300

    addon.js

--- addon.js
+++ addon.js
@@ -3562,6 +3562,66 @@ Lampa.SettingsApi.addComponent({
 					}
 		});
 
+		        Lampa.SettingsApi.addParam({
+					component: 'add_torrent_plugin',
+					param: {
+						name: 'free_torr',
+						type: 'select',
+						values: {
+							1:	'Установить',
+							2:	'Удалить',
+						},
+					//default: '1',
+					},
+					field: {
+						name: 'Free Torrserver',
+						description: 'Плагин автоматически подставляет torrserver из своей базы'
+					},
+					onChange: function(value) {
+						if (value == '1') {
+							itemON('https://amikdn.github.io/freet.js', 'Free Torrserver', '@amik', 'free_torr', nthChildIndex);
+							// console.log("nthChildIndex, переданный в itemON:", nthChildIndex);
+						}
+						if (value == '2') {
+							var pluginToRemoveUrl = "https://amikdn.github.io/freet.js";
+							deletePlugin(pluginToRemoveUrl, nthChildIndex);
+							// console.log("nthChildIndex, переданный в deletePlugin:", nthChildIndex);
+                                                }
+					},
+					onRender: function (item) {$('.settings-param__name', item).css('color','f3d900'); hideInstall();
+						/*var myResult = checkPlugin('https://amikdn.github.io/freet.js')
+						setTimeout(function() {	
+							$('div[data-name="free_torr"]').append('<div class="settings-param__status one"></div>')
+							if (myResult) {
+								$('div[data-name="free_torr"]').find('.settings-param__status').removeClass('active error wait').addClass('active')
+							} else {
+								$('div[data-name="free_torr"]').find('.settings-param__status').removeClass('active error wait').addClass('error')
+							}
+						}, 100);*/
+						var myResult = checkPlugin('https://amikdn.github.io/freet.js');
+                                                var pluginsArray = Lampa.Storage.get('plugins');
+                                                    setTimeout(function() {
+                                                       $('div[data-name="free_torr"]').append('<div class="settings-param__status one"></div>');
+                                                       var pluginStatus = null;
+                                                       for (var i = 0; i < pluginsArray.length; i++) {
+                                                          if (pluginsArray[i].url === 'https://amikdn.github.io/freet.js') {
+                                                             pluginStatus = pluginsArray[i].status;
+                                                             break;
+                                                          }
+                                                       }
+                                                       if (myResult && pluginStatus !== 0) {
+                                                          $('div[data-name="free_torr"]').find('.settings-param__status').removeClass('active error').addClass('active');
+                                                       } else if (pluginStatus === 0) {
+                                                          $('div[data-name="free_torr"]').find('.settings-param__status').removeClass('active error').css('background-color', 'rgb(255, 165, 0)');
+                                                       } else {
+                                                          $('div[data-name="free_torr"]').find('.settings-param__status').removeClass('active error').addClass('error');
+                                                       }
+                                                    }, 100);
+						    item.on("hover:enter", function (event) {
+                                                        nthChildIndex = focus_back(event); // Сохраняем элемент в переменной
+						    });
+					}
+		});
 
 	        Lampa.SettingsApi.addParam({
 					component: 'add_torrent_plugin',
