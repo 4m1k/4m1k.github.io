d17408b39da3a1c9432757cfa838db695e25b5d4
lysiy <vazonez1982@gmail.com>
2026-06-22 20:58:51 +0300

    Update jackett.js

--- jackett.js
+++ jackett.js
@@ -11,10 +11,11 @@
     { id: 'jacred_pro', name: 'RU Jacred Pro', baseUrl: 'ru.jacred.pro', key: '', interview: 'all', lang: 'lg' },
     { id: 'tribal', name: 'Tribal', baseUrl: '11.307407.xyz', protocol: 'https://', key: '', interview: 'all', lang: 'lg' },
     { id: 'freebie_tom_ru', name: 'Freebie', baseUrl: 'jacred.freebie.tom.ru', key: '1', interview: 'all', lang: 'lg' },
+    // { id: 'jac_black', name: 'Jac Black', baseUrl: 'jacblack.ru:9117', key: '', interview: 'all', lang: 'lg' },
     { id: 'Myjacket', name: 'Myjacket', baseUrl: 'lampac.fun:8117', key: 'cvy139co64s9pu791s2ao7egzzgogocw', interview: 'all', lang: 'lg' }
   ];
 
-  var PARSER_ICON = '<svg width="24" height="24" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"> <polygon fill="#074761" points="187.305,27.642 324.696,27.642 256,236.716"/> <polygon fill="#10BAFC" points="187.305,27.642 256,236.716 163.005,151.035 196.964,151.035 110.934,49.96"/> <polygon fill="#0084FF" points="66.917,62.218 10.45,434.55 66.917,451.922 117.726,217.908"/> <polygon fill="#0084FF" points="163.005,151.035 196.964,151.035 110.934,49.96 66.917,62.218 117.726,217.908 117.726,484.356 256,484.356 256,236.716"/> <polygon fill="#10BAFC" points="324.696,27.642 256,236.716 348.996,151.035 315.037,151.035 401.067,49.96"/> <polygon fill="#0084FF" points="445.084,62.218 501.551,434.55 445.084,451.922 394.275,217.908"/> <polygon fill="#0084FF" points="348.996,151.035 315.037,151.035 401.067,49.96 445.084,62.218 394.275,217.908 394.275,484.356 256,484.356 256,236.716"/> <path fill="#000000" d="M291.559,308.803c-7.49,0-13.584-6.094-13.584-13.584c0-7.49,6.094-13.584,13.584-13.584s13.584,6.094,13.584,13.584 C305.143,302.71,299.049,308.803,291.559,308.803z"/> <path fill="#000000" d="M291.559,427.919c-7.49,0-13.584-6.094-13.584-13.584s6.094-13.584,13.584-13.584s13.584,6.094,13.584,13.584 S299.049,427.919,291.559,427.919z"/> <path fill="#000000" d="M291.559,368.405c-7.49,0-13.584-6.094-13.584-13.584s6.094-13.584,13.584-13.584s13.584,6.094,13.584,13.584 S299.049,368.405,291.559,368.405z"/> <path fill="#000000" d="M225.677,424.785h-4.678c-5.77,0-10.449-4.679-10.449-10.449s4.679-10.449,10.449-10.449h4.678 c5.771,0,10.449,4.679,10.449,10.449S231.448,424.785,225.677,424.785z"/> <path fill="#000000" d="M384.063,220.125c8.948-1.219,5.008,7.842,10.646,6.617c5.637-1.225,8.551-16.691,9.775-11.052"/> <path fill="#000000" d="M511.881,432.984L455.414,60.652c-0.004-0.001-0.008-0.001-0.013-0.002c-0.178-1.166-0.541-2.306-1.109-3.367 c-1.346-2.513-3.66-4.367-6.407-5.131L327.627,17.613c-0.976-0.284-1.961-0.416-2.931-0.416c0-0.001-137.391-0.001-137.391-0.001 c-0.97,0.001-1.955,0.106-2.931,0.39L59.125,51.994c-2.747,0.764-5.061,2.618-6.407,5.131c-0.568,1.061-0.931,2.201-1.109,3.367 c-0.005,0.001-0.009,0.001-0.013,0.002L0.119,432.984c-0.427,2.799,0.384,5.65,2.201,7.792c1.817,2.143,4.494,3.375,7.324,3.375h492.711 c2.83,0,5.507-1.232,7.324-3.375C511.497,438.634,512.308,435.783,511.881,432.984z M225.677,393.506h-4.678 c-5.77,0-10.449-4.679-10.449-10.449s4.679-10.449,10.449-10.449h4.678c5.771,0,10.449,4.679,10.449,10.449 S231.448,393.506,225.677,393.506z M225.677,362.227h-4.678c-5.77,0-10.449-4.679-10.449-10.449s4.679-10.449,10.449-10.449h4.678 c5.771,0,10.449,4.679,10.449,10.449S231.448,362.227,225.677,362.227z M291.559,274.399c-7.49,0-13.584-6.094-13.584-13.584 c0-7.49,6.094-13.584,13.584-13.584s13.584,6.094,13.584,13.584C305.143,268.305,299.049,274.399,291.559,274.399z M291.559,355.96 c-7.49,0-13.584-6.094-13.584-13.584s6.094-13.584,13.584-13.584s13.584,6.094,13.584,13.584S299.049,355.96,291.559,355.96z M291.559,415.474c-7.49,0-13.584-6.094-13.584-13.584s6.094-13.584,13.584-13.584s13.584,6.094,13.584,13.584 S299.049,415.474,291.559,415.474z"/></svg>';
+  var PARSER_ICON = '<svg width="24" height="24" viewBox="0 0 512 512" xmlns="http://www.w3.org/2000/svg"> <polygon fill="#074761" points="187.305,27.642 324.696,27.642 256,236.716"/> <polygon fill="#10BAFC" points="187.305,27.642 256,236.716 163.005,151.035 196.964,151.035 110.934,49.96"/> <polygon fill="#0084FF" points="66.917,62.218 10.45,434.55 66.917,451.922 117.726,217.908"/> <polygon fill="#0084FF" points="163.005,151.035 196.964,151.035 110.934,49.96 66.917,62.218 117.726,217.908 117.726,484.356 256,484.356 256,236.716"/> <polygon fill="#10BAFC" points="324.696,27.642 256,236.716 348.996,151.035 315.037,151.035 401.067,49.96"/> <polygon fill="#0084FF" points="445.084,62.218 501.551,434.55 445.084,451.922 394.275,217.908"/> <polygon fill="#0084FF" points="348.996,151.035 315.037,151.035 401.067,49.96 445.084,62.218 394.275,217.908 394.275,484.356 256,484.356 256,236.716"/> <path fill="#000000" d="M291.559,308.803c-7.49,0-13.584-6.094-13.584-13.584c0-7.49,6.094-13.584,13.584-13.584s13.584,6.094,13.584,13.584 C305.143,302.71,299.049,308.803,291.559,308.803z"/> <path fill="#000000" d="M291.559,427.919c-7.49,0-13.584-6.094-13.584-13.584s6.094-13.584,13.584-13.584s13.584,6.094,13.584,13.584 S299.049,427.919,291.559,427.919z"/> <path fill="#000000" d="M291.559,368.405c-7.49,0-13.584-6.094-13.584-13.584s6.094-13.584,13.584-13.584s13.584,6.094,13.584,13.584 S299.049,368.405,291.559,368.405z"/> <path fill="#000000" d="M225.677,424.785h-4.678c-5.77,0-10.449-4.679-10.449-10.449s4.679-10.449,10.449-10.449h4.678 c5.771,0,10.449,4.679,10.449,10.449S231.448,424.785,225.677,424.785z"/> <path fill="#000000" d="M384.063,220.125c8.948-1.219,5.008,7.842,10.646,6.617c5.637-1.225,8.551-16.691,9.775-11.052"/> <path fill="#000000" d="M511.881,432.984L455.414,60.652c-0.004-0.001-0.008-0.001-0.013-0.002c-0.178-1.166-0.541-2.306-1.109-3.367 c-1.346-2.513-3.66-4.367-6.407-5.131L327.627,17.613c-0.976-0.284-1.961-0.416-2.931-0.416c0-0.001-137.391-0.001-137.391-0.001 c-0.97,0.001-1.955,0.132-2.931,0.417L64.114,52.152c-2.747,0.766-5.061,2.619-6.407,5.131c-0.569,1.064-0.933,2.208-1.11,3.377 c-0.004-0.002-0.007-0.006-0.011-0.009L0.119,432.984c-0.776,5.117,2.311,10.032,7.258,11.553l56.467,17.371 c1.005,0.309,2.041,0.462,3.072,0.462c1.836,0,3.659-0.484,5.276-1.429c2.524-1.476,4.315-3.943,4.936-6.802l30.149-138.858v169.075 c0,5.771,4.679,10.449,10.449,10.449h276.548c5.77,0,10.449-4.678,10.449-10.449V315.281l30.148,138.858 c0.621,2.858,2.412,5.326,4.936,6.802c1.616,0.946,3.44,1.429,5.276,1.429c1.031,0,2.067-0.154,3.072-0.462l56.467-17.371 C509.571,443.015,512.658,438.101,511.881,432.984z M331.467,40.507l51.19,14.959l-75.578,88.795 c-2.64,3.102-3.237,7.457-1.529,11.155c1.709,3.698,5.411,6.067,9.486,6.067h7.198l-43.765,40.324L331.467,40.507z M180.533,40.507 l52.998,161.3l-43.765-40.324h7.198c4.074,0,7.776-2.369,9.486-6.067c1.708-3.698,1.112-8.053-1.529-11.155l-75.578-88.795 L180.533,40.507z M59.119,438.59l-36.987-11.379l48.512-319.89l36.269,111.136L59.119,438.59z M245.552,473.907H128.175v-49.123 h59.02c5.77,0,10.449-4.679,10.449-10.449s-4.679-10.449-10.449-10.449h-59.02V217.908c0-1.101-0.174-2.195-0.515-3.242 L80.238,69.355l27.068-7.539l67.043,78.769h-11.343c-4.304,0-8.168,2.638-9.733,6.649c-1.565,4.009-0.512,8.568,2.653,11.484 l89.627,82.578L245.552,473.907L245.552,473.907z M201.736,38.092h108.528L256,203.243L201.736,38.092z M384.341,214.666 c-0.341,1.047-0.515,2.141-0.515,3.242v255.999H266.449V241.297l89.627-82.578c3.165-2.916,4.218-7.475,2.653-11.484 c-1.565-4.01-5.429-6.649-9.733-6.649h-11.343l67.043-78.769l27.068,7.539L384.341,214.666z M452.882,438.59l-47.795-220.132 l36.268-111.136l48.515,319.89L452.882,438.59z"/> <path fill="#000000" d="M353.197,262.86h-61.637c-5.77,0-10.449-4.679-10.449-10.449c0-5.771,4.679-10.449,10.449-10.449h61.637 c5.77,0,10.449,4.678,10.449,10.449C363.646,258.182,358.968,262.86,353.197,262.86z"/> </svg>';
 
   var COLOR_OK = '#64e364';
   var COLOR_FAIL = '#ff2121';
@@ -93,6 +94,33 @@
     xhr.onerror = function () { callback(server, false); };
     xhr.open('GET', url, true);
     xhr.send();
+  }
+
+  function updateServerStatusInSettings() {
+    setTimeout(function () {
+      var firstItem = $('body > div.selectbox > div.selectbox__content.layer--height > div.selectbox__body.layer--wheight > div > div > div > div:nth-child(1) > div');
+      if (firstItem.text().trim() !== 'Свой вариант') return;
+
+      servers.forEach(function (server, index) {
+        var selector = 'body > div.selectbox > div.selectbox__content.layer--height > div.selectbox__body.layer--wheight > div > div > div > div:nth-child(' + (index + 2) + ') > div';
+        var element = $(selector);
+        if (element.text().trim() !== server.name) return;
+
+        if (isAlwaysOnline(server)) {
+          element.html('✓&nbsp;&nbsp;' + server.name).css('color', COLOR_OK);
+          return;
+        }
+
+        checkServerStatus(server, function (srv, ok, status) {
+          if (ok) {
+            element.html('✓&nbsp;&nbsp;' + srv.name).css('color', COLOR_OK);
+          } else {
+            var color = status === 401 ? COLOR_AUTH : COLOR_FAIL;
+            element.html('✗&nbsp;&nbsp;' + srv.name).css('color', color);
+          }
+        });
+      });
+    }, 1000);
   }
 
   var selectValues = { no_parser: 'Свой вариант' };
@@ -200,7 +228,7 @@
   function scheduleParserButtonAfterChange() {
     if (parserButtonRetryInterval) clearInterval(parserButtonRetryInterval);
     var attempts = 0;
-    var maxAttempts = 40;
+    var maxAttempts = 40; // 40 * 400ms = 16 сек
 
     parserButtonRetryInterval = setInterval(function () {
       addParserFilterButton();
@@ -339,7 +367,6 @@
           var er = target.getBoundingClientRect();
           var vr = viewportEl.getBoundingClientRect();
           if (!er.height || !vr.height) return;
-
           if (er.bottom > vr.bottom - EDGE_SCROLL_PAD_J) {
             scroll.wheel(er.bottom - vr.bottom + EDGE_SCROLL_PAD_J);
           } else if (er.top < vr.top + EDGE_SCROLL_PAD_J) {
@@ -393,27 +420,6 @@
     var list = $('<div class="jackett-server-list"></div>');
     var rowMap = {};
 
-    var noParserRow = $('<div class="selector jackett-server-item" tabindex="0">' +
-      '<div class="jackett-server-info">' +
-      '<div class="jackett-server-name">Свой вариант</div>' +
-      '</div>' +
-      '<div class="jackett-server-status"></div>' +
-      '</div>');
-
-    if (currentSelected === 'no_parser') noParserRow.addClass('jackett-server-active');
-
-    noParserRow.on('hover:enter', function () {
-      Lampa.Storage.set('jackett_urltwo', 'no_parser');
-      applyServerConfig();
-      closeModalSafeJ();
-      $('div[data-name="jackett_url"]').show();
-      $('div[data-name="jackett_key"]').show();
-      Lampa.Settings.update();
-      closeParserSelectAndRestore(controllerBeforeModal);
-    });
-
-    list.append(noParserRow);
-
     servers.forEach(function (s) {
       (function (server) {
         var isActive = (server.id === currentSelected);
@@ -429,8 +435,6 @@
         row.on('hover:enter', function () {
           var item = getServerSelectItem(server);
           closeModalSafeJ();
-          $('div[data-name="jackett_url"]').hide();
-          $('div[data-name="jackett_key"]').hide();
           applyParserAndRefreshTorrents(item, currentActivity);
         });
 
@@ -512,7 +516,7 @@
       parserButtonObserver = null;
     }
   }
-
+  
   Lampa.Storage.listener.follow('change', function (e) {
     if (e.name === 'parser_torrent_type') {
       var el = $('div[data-name="jackett_urltwo"]');
@@ -536,6 +540,12 @@
     if (e.name === 'jackett_urltwo') {
       var nameEl = document.getElementById('current-parser-name');
       if (nameEl) nameEl.textContent = getCurrentParserName();
+    }
+  });
+
+  Lampa.Controller.listener.follow('toggle', function (e) {
+    if (e.name === 'select') {
+      setTimeout(updateServerStatusInSettings, 10);
     }
   });
 
