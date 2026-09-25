fbb79ae7005fad06874bf0dd6fa95a5ff3adc457
lysiy <vazonez1982@gmail.com>
2026-09-25 21:00:18 +0300

    Update cuboff.js

--- cuboff.js
+++ cuboff.js
@@ -4,15 +4,46 @@
 
   (function () {
     function initLampaHook() {
-      if (window.Lampa && Lampa.Player && Lampa.Player.play) {
-        var originalPlay = Lampa.Player.play;
-        Lampa.Player.play = function (object) {
-          object.iptv = true;
+      if (window.Lampa && Lampa.Player && Lampa.Player.listener) {
+        $.ajaxTransport('+json', function (options) {
+          if (/\/api\/ad\/get\/banner(?:[?#]|$)/.test(options.url)) {
+            return {
+              send: function (headers, complete) {
+                complete(200, 'OK', { json: { ad: [] } });
+              },
+              abort: function () {}
+            };
+          }
+        });
 
-          if (object.vast_url) delete object.vast_url;
-          if (object.vast_msg) delete object.vast_msg;
-          
-          return originalPlay.apply(this, arguments);
+        var listener = Lampa.Player.listener;
+        var originalSend = listener.send;
+        var restore;
+
+        listener.send = function (type, event) {
+          if (restore && (type === 'create' || type === 'start' || type === 'external' || type === 'destroy')) {
+            restore();
+            restore = null;
+          }
+
+          var result = originalSend.apply(this, arguments);
+
+          if (type === 'create' && event && event.data) {
+            var data = event.data;
+            var hadIptv = Object.prototype.hasOwnProperty.call(data, 'iptv');
+            var iptv = data.iptv;
+
+            restore = function () {
+              if (hadIptv) data.iptv = iptv;
+              else delete data.iptv;
+            };
+
+            data.iptv = true;
+            delete data.vast_url;
+            delete data.vast_msg;
+          }
+
+          return result;
         };
       } else {
         setTimeout(initLampaHook, 500);
